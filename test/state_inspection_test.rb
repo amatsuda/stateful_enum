944f13afce72fe7df195fb0728f32d999cb93da1
Jairo Vazquez <jairovm20@gmail.com>
2020-04-13 16:24:29 -0700

    Fixes tests

--- test/state_inspection_test.rb
+++ test/state_inspection_test.rb
@@ -4,17 +4,20 @@ require 'test_helper'
 
 class StateInspectionTest < ActiveSupport::TestCase
   def test_possible_events
-    unassigned_bug = Bug.new status: :assigned, assigned_to: User.create!(name: 'matz')
+    unassigned_bug = Bug.new assigned_to: User.create!(name: 'matz')
+    unassigned_bug.write_attribute(:status, :assigned)
     assert_equal %i[resolve close], unassigned_bug.stateful_enum.possible_events.map(&:value_method_name)
   end
 
   def test_possible_event_names
-    unassigned_bug = Bug.new status: :assigned, assigned_to: User.create!(name: 'matz')
+    unassigned_bug = Bug.new assigned_to: User.create!(name: 'matz')
+    unassigned_bug.write_attribute(:status, :assigned)
     assert_equal %i[resolve close], unassigned_bug.stateful_enum.possible_event_names
   end
 
   def test_possible_states
-    unassigned_bug = Bug.new status: :assigned, assigned_to: User.create!(name: 'matz')
+    unassigned_bug = Bug.new assigned_to: User.create!(name: 'matz')
+    unassigned_bug.write_attribute(:status, :assigned)
     assert_equal %i[resolved closed], unassigned_bug.stateful_enum.possible_states
   end
 
